4c1db4d611358cf5712ef5b9af351dc573c78c68
Ali-Abbas-Personal <Ali.Abbas.SSJ1999@gmail.com>
2023-03-16 10:57:11 +0200

    register the datasets before retrieving the configurations object

--- train.py
+++ train.py
@@ -5,6 +5,8 @@ import argparse
 def train(network_base_name: str,
           weights_path: str,
           yaml_url: str,
+          thing_classes: list,
+          data_directory: str = 'data',
           initial_learning_rate: float = 0.00025,
           train_steps: int = 5000,
           eval_steps: int = 5000,
@@ -17,6 +19,8 @@ def train(network_base_name: str,
     :param network_base_name: The name of the network base to use.
     :param weights_path: The path to the weights file.
     :param yaml_url: The URL to the YAML configuration file.
+    :param thing_classes: list, a list of strings representing the classes in the dataset
+    :param data_directory: str, the directory that holds the datasets
     :param initial_learning_rate: The initial learning rate to use for training. Default is 0.00025.
     :param train_steps: The number of training steps to perform. Default is 5000.
     :param eval_steps: The number of evaluation steps to perform. Default is 5000.
@@ -25,6 +29,9 @@ def train(network_base_name: str,
     :param learning_rate_decay_factor: The factor by which to decay the learning rate. Default is .9.
 
     """
+
+    # register the datasets
+    register_datasets(data_directory=data_directory, thing_classes=thing_classes)
 
     # Get configurations from specified parameters
     configurations = get_cfg(network_base_name=network_base_name,
@@ -49,6 +56,8 @@ if __name__ == '__main__':
     parser.add_argument('--network_base_name', type=str, required=True)
     parser.add_argument('--weights_path', type=str, required=True)
     parser.add_argument('--yaml_url', type=str, required=True)
+    parser.add_argument('--data_directory', type=str, default='data')
+    parser.add_argument('--thing_classes', type=list, default=['face', 'license_plate'])
     parser.add_argument('--initial_learning_rate', type=float, default=0.00025)
     parser.add_argument('--train_steps', type=int, default=5000)
     parser.add_argument('--eval_steps', type=int, default=5000)

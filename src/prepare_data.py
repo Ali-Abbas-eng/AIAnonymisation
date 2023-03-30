adec3e79a640e2424a5ba1b201b62e4221483ca8
Ali Abbas <ali.abbas@lit-co.net>
2023-03-30 11:05:37 +0200

    split the download_and_extract functionality from the generate_registration_files functionality

--- src/prepare_data.py
+++ src/prepare_data.py
@@ -1,31 +1,42 @@
 import ccpd_2019
 import celeba
+import wider_face
+import argparse
 from data_tools import *
 
 
+def download_and_extract(compressed_files_directory: str or os.PathLike,
+                         data_directory: str or os.PathLike = os.path.join('../data', 'raw')):
+    ccpd_2019.download_and_extract(download_directory=compressed_files_directory,
+                                   unzipped_directory=data_directory)
+    celeba.download_and_extract(download_directory=os.path.join(compressed_files_directory, 'img_celeba.7z'),
+                                unzipped_directory=CELEB_A_IMAGES_DIRECTORY)
+    wider_face.download_and_extract(download_directory=os.path.join(compressed_files_directory, 'WIDER_FACE'),
+                                    unzipped_directory=os.path.join(data_directory, 'WIDER_FACE'))
+
+
 def main(compressed_files_directory: str or os.PathLike,
-         data_directory: str or os.PathLike = os.path.join('../data', 'raw')):
+         data_directory: str or os.PathLike = os.path.join('../data', 'raw'),
+         download: bool = False):
     """
     Encapsulation of the data retrieval process (Downloads, extracts, and then generates the final dataset to be used.
     :param compressed_files_directory: str, the base directory to which downloaded files will be saved.
     :param data_directory: str, the base directory to which extracted images will be saved
+    :param download: bool, whither to download the datasets as zip files.
     :return:
     """
-    ccpd_2019.download_and_extract(download_directory=compressed_files_directory,
-                                   unzipped_directory=data_directory)
+    if download:
+        download_and_extract(compressed_files_directory=compressed_files_directory,
+                             data_directory=data_directory)
+
     ccpd_2019.generate_dataset_registration_info(data_directory=CCPD_IMAGES_DIRECTORY,
                                                  info_path=CCPD_INFORMATION_FILE,
                                                  create_record=create_record)
 
-    celeba.download_and_extract(download_directory=os.path.join(compressed_files_directory, 'img_celeba.7z'),
-                                unzipped_directory=CELEB_A_IMAGES_DIRECTORY)
     celeba.generate_dataset_registration_info(data_directory=CELEB_A_IMAGES_DIRECTORY,
                                               annotations_file=CELEB_A_ANNOTATIONS_FILE,
                                               info_path=CELEB_A_INFORMATION_FILE,
                                               create_record=create_record)
-
-    wider_face.download_and_extract(download_directory=os.path.join(compressed_files_directory, 'WIDER_FACE'),
-                                    unzipped_directory=os.path.join(data_directory, 'WIDER_FACE'))
 
     wider_face.write_data(data_directory_train=WIDER_FACE_IMAGES_DIRECTORY_TRAIN,
                           data_directory_valid=WIDER_FACE_IMAGES_DIRECTORY_VALID,
@@ -39,6 +50,7 @@ def main(compressed_files_directory: str or os.PathLike,
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--download', type=bool, default=False)
     parser.add_argument('--compressed_files_directory', type=str, default=os.path.join('../data', 'zipped'))
     parser.add_argument('--data_directory', type=str, default=os.path.join('../data', 'raw'))
     args = vars(parser.parse_args())

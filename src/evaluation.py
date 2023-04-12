c16a001e1757e8a6795cb7c406c21257a0a6bb99
Ali Abbas <ali.abbas@lit-co.net>
2023-04-12 09:17:34 +0300

    added logger to the evaluation process. added the option to choose a device other than GPU for evaluation (extremely not recommended unless you're trying to fill the time gap between training and testing).

--- src/evaluation.py
+++ src/evaluation.py
@@ -8,17 +8,22 @@ from detectron2.evaluation import COCOEvaluator, inference_on_dataset
 from detectron2.data import build_detection_test_loader
 from detectron2.model_zoo import get_config_file
 import json
+from detectron2.utils.logger import setup_logger
+setup_logger()
 
 
 def evaluate(yaml_url: str or os.PathLike,
              model_weights: str or os.PathLike,
              test_data_file: str or os.PathLike,
-             output_dir: str or os.PathLike):
+             output_dir: str or os.PathLike,
+             device: str):
+    if device == 'cpu':
+        os.environ['CUDA_VISIBLE_DEVICES'] = ''
+        # Set device to CPU
+        torch.set_grad_enabled(False)
+        torch.set_num_threads(1)
 
-    # Set device to CPU
-    torch.set_grad_enabled(False)
-    torch.set_num_threads(1)
-    device = torch.device("cpu")
+    device = torch.device(device)
 
     # Load test data in COCO format from a JSON file
     test_data = json.load(open(test_data_file, 'r'))
@@ -42,7 +47,7 @@ def evaluate(yaml_url: str or os.PathLike,
     predictor = DefaultPredictor(cfg)
 
     # Evaluate model on test dataset
-    evaluator = COCOEvaluator(test_dataset_name, cfg, False, output_dir=os.path.join(output_dir, base_name, 'test'))
+    evaluator = COCOEvaluator(test_dataset_name, output_dir=os.path.join(output_dir, base_name, 'test'))
     val_loader = build_detection_test_loader(cfg, test_dataset_name)
     inference_on_dataset(predictor.model, val_loader, evaluator)
 
@@ -53,5 +58,6 @@ if __name__ == '__main__':
     parser.add_argument('--model_weights', type=str, required=True)
     parser.add_argument('--test_data_file', type=str, default=os.path.join('data', 'test.json'))
     parser.add_argument('--output_dir', type=str, default='output')
+    parser.add_argument('--device', type=str, default='cuda')
     args = vars(parser.parse_args())
     evaluate(**args)

b14635cbaf625f76c5c59f3b5f3d1010d69be262
Ali-Abbas <ali.abbas@lit-co.net>
2023-04-26 09:19:29 +0300

    * removed requiring yaml_url and replaced with the more convenient way of specifying the model's base name and then inferring the yaml_url since all the models of interest share the same yaml url pattern.
    * saved a visualisation plot to the same directory as the dataset's json information file (same name different extension).
    
    * changed some default command line arguments values.
    
    * ran the whole training inside the catch warning clause so the only output is from print statements and detectron2 logger.

--- src/train.py
+++ src/train.py
@@ -1,3 +1,5 @@
+import warnings
+
 from data_tools import register_dataset, visualize_sample
 from training_utils import get_cfg, Trainer
 import os
@@ -5,9 +7,9 @@ import argparse
 from evaluation import evaluate
 
 
-def train(yaml_url: str,
+def train(network_base_name: str,
           train_files: list,
-          test_files: list,
+          valid_files: list,
           initial_learning_rate: float,
           train_steps: int,
           eval_steps: int,
@@ -18,9 +20,9 @@ def train(yaml_url: str,
           freeze_at: int):
     """
     Trains a model using the specified configurations.
-    :param yaml_url: The URL to the YAML configuration file.
+    :param network_base_name: The URL to the YAML configuration file.
     :param train_files: list, list of json files containing train dataset catalogues to be registered.
-    :param test_files: list, list of json files containing train dataset catalogues to be registered.
+    :param valid_files: list, list of json files containing train dataset catalogues to be registered.
     :param initial_learning_rate: The initial learning rate to use for training. Default is 0.00025.
     :param train_steps: The number of training steps to perform. Default is 120_000.
     :param eval_steps: The number of evaluation steps to perform. Default is 50_000.
@@ -32,17 +34,16 @@ def train(yaml_url: str,
     output layer, -1 means train all).
     """
     # register the datasets
-    train_datasets = [file.split('/')[-1].replace('.json', '') for file in train_files]
-    test_datasets = [file.split('/')[-1].replace('.json', '') for file in test_files]
+    train_sets = [file.split('/')[-1].replace('.json', '') for file in train_files]
+    valid_sets = [file.split('/')[-1].replace('.json', '') for file in valid_files]
 
-    [register_dataset(file, file.split('/')[-1].replace('.json', '')) for file in [*train_files, *test_files]]
-    [visualize_sample(file) for file in [*train_files, *test_files]]
-    network_base_name = yaml_url.split('/')[-1].replace('.yaml', '')
+    [register_dataset(file, file.split('/')[-1].replace('.json', '')) for file in [*train_files, *valid_files]]
+    yaml_url = f'COCO-Detection/{network_base_name}.yaml'
     # Get configurations from specified parameters
     configurations = get_cfg(network_base_name=network_base_name,
                              yaml_url=yaml_url,
-                             train_datasets=tuple(train_datasets),
-                             test_datasets=tuple(test_datasets),
+                             train_datasets=tuple(train_sets),
+                             test_datasets=tuple(valid_sets),
                              decay_gamma=decay_gamma,
                              initial_learning_rate=initial_learning_rate,
                              train_steps=train_steps,
@@ -50,6 +51,9 @@ def train(yaml_url: str,
                              batch_size=batch_size,
                              output_directory=output_directory,
                              freeze_at=freeze_at)
+
+    [visualize_sample(file, show=True, save_path=file.replace('json', 'png'))
+     for file in [*train_files, *valid_files]]
     # Create trainer object with configurations
     trainer = Trainer(configurations)
 
@@ -59,7 +63,7 @@ def train(yaml_url: str,
     # Train model
     trainer.train()
 
-    evaluate(yaml_url=yaml_url,
+    evaluate(yaml_url=network_base_name,
              model_weights=os.path.join(output_directory, network_base_name, 'model_final.pth'),
              test_data_file=os.path.join('data', 'test.json'),
              output_dir=os.path.join(output_directory, network_base_name, 'test'),
@@ -68,20 +72,21 @@ def train(yaml_url: str,
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--yaml_url', type=str, required=True)
+    parser.add_argument('--network_base_name', type=str, required=True)
     parser.add_argument('--train_files', nargs='+', default=['data/raw/CelebA/celeba_train.json',
                                                              'data/raw/CCPD2019/ccpd_train.json'])
-    parser.add_argument('--test_files', nargs='+', default=['data/val.json'])
+    parser.add_argument('--valid_files', nargs='+', default=['data/val.json'])
     parser.add_argument('--decay_gamma', type=float, default=0.1)
-    parser.add_argument('--output_directory', type=str, default='output')
+    parser.add_argument('--output_directory', type=str, default='output_trial_003')
     parser.add_argument('--initial_learning_rate', type=float, default=0.00025)
     parser.add_argument('--train_steps', type=int, default=80_000)
-    parser.add_argument('--eval_steps', type=int, default=50_000)
-    parser.add_argument('--batch_size', type=int, default=8)
+    parser.add_argument('--eval_steps', type=int, default=10_000)
+    parser.add_argument('--batch_size', type=int, default=4)
     parser.add_argument('--eval_device', type=str, default='cuda')
     parser.add_argument('--freeze_at', type=int, default=0)
 
     args = vars(parser.parse_args())
-
-    train(**args)
+    with warnings.catch_warnings():
+        warnings.simplefilter('ignore')
+        train(**args)
 

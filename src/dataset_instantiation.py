9623fe6ced939054a75901046e4177d6425d182b
Ali-Abbas <ali.abbas@lit-co.net>
2023-05-23 08:50:03 +0300

    added the fddb dataset

--- src/dataset_instantiation.py
+++ src/dataset_instantiation.py
@@ -1,9 +1,10 @@
-from .dataset import ImagesDataset
+from src.dataset import ImagesDataset
 import os
-from .data_tools import create_record
+from src.data_tools import create_record
 from tqdm.auto import tqdm
-from .data_tools import path_fixer
+from src.data_tools import path_fixer
 import itertools
+from typing import Callable
 
 
 def get_ccpd2019_dataset():
@@ -108,6 +109,7 @@ def get_celeba_dataset():
             # Create an empty list to store the dataset records
             dataset_records = [generate_data_point(index, line) for index, line in enumerate(annotations)]
             return dataset_records
+
     urls = {
         'img_celba.7z': 'https://drive.google.com/drive/folders/1eyJ52hX5KpniS9MB-MFXBRtJUDpeldqx?usp=share_link',
         'list_bbox_celeba.txt': 'https://drive.google.com/uc?id=19X0GE3kP6tNatS9kZ2-Ks2_OeeCtqeFI'
@@ -151,7 +153,7 @@ def get_wider_face_dataset():
                     idx = annotations.index(row)
                     # Get the file internal path and image path
                     file_internal_path = path_fixer(row)
-                    image_path = os.path.join(data_directory, 'images',file_internal_path)
+                    image_path = os.path.join(data_directory, 'images', file_internal_path)
 
                     # Get the number of images and bounding boxes
                     num_faces = int(annotations[idx + 1])
@@ -193,3 +195,126 @@ def get_wider_face_dataset():
     }
     wider_face_default.registration_info_generator = generate_dataset_registration_info
     return wider_face_default
+
+
+def get_fddb_dataset():
+    def generate_dataset_registration_info(data_directory: str or os.PathLike):
+        """
+        Generates dataset registration information and saves it as a JSON file.
+
+        Args:
+            data_directory (str or os.PathLike): Directory path of the dataset.
+        """
+
+        def get_bbox(details):
+            """
+            Calculates the bounding box coordinates from the given face details.
+
+            Args:
+                details (list): List containing face details.
+
+            Returns:
+                tuple: Bounding box coordinates (x1, y1, x2, y2).
+            """
+            # Extract the face details and convert them to integers
+            face = [int(float(details[j])) for j in range(5)]
+
+            # Calculate the center coordinates of the bounding box
+            centre = (face[3], face[4])
+
+            # Calculate the lengths of the axes of the bounding box
+            axes_length = (face[1], face[0])
+
+            # Calculate the corner coordinates of the bounding box
+            x1, y1 = centre[0] + axes_length[0], centre[1] - axes_length[1]
+            x2, y2 = centre[0] - axes_length[0], centre[1] + axes_length[1]
+
+            # Return the bounding box coordinates
+            return x1, y1, x2, y2
+
+        def extract_file_info(data_directory,
+                              text_file: str or os.PathLike,
+                              create_rec: Callable,
+                              progress_bar):
+            """
+            Extracts file information from a text file and generates records based on the information.
+
+            Args:
+                data_directory (str or os.PathLike): Directory path of the data files.
+                text_file (str or os.PathLike): Path to the text file containing file information.
+                create_rec (Callable): A function that creates a record based on given parameters.
+                progress_bar: A progress bar object used to track progress.
+
+            Returns:
+                list: List of records generated from the file information.
+            """
+            # Read the text file and split it into lines
+            with open(text_file) as file_handle:
+                info = file_handle.read().split('\n')
+                info = [item for item in info if item != '\n']
+
+            image_index = 0
+            records = []
+            for index, line in enumerate(info):
+                # Construct the full path of the image file
+                path = path_fixer(os.path.join(data_directory, line)) + '.jpg'
+
+                coordinates = []
+                if os.path.exists(path):
+                    num_faces = int(info[index + 1])
+                    faces = info[index + 2: index + 2 + num_faces]
+                    for face in faces:
+                        details = face.split()
+                        # Extract the bounding box coordinates using the get_bbox function
+                        x1, y1, x2, y2 = get_bbox(details=details)
+                        coordinates.extend([x1, y1, x2, y2])
+
+                    # Create a record using the provided create_record function
+                    record = create_rec(image_path=path,
+                                        bounding_boxes=coordinates,
+                                        category_id=0,
+                                        index=image_index)
+                    image_index += 1
+                    progress_bar.update()
+                    records.append(record)
+
+            return records
+
+        # Calculate the total progress based on the number of files to process
+        total_progress = sum([len(open(os.path.join(data_directory, 'FDDB-folds', file)).read().split('\n'))
+                              for file in os.listdir(os.path.join(data_directory, 'FDDB-folds'))
+                              if 'ellipse' not in file]) - 10
+
+        # Initialize a progress bar with the total progress
+        with tqdm(total=total_progress, desc='Generating FDDB Registration Info') as progress_bar:
+            total_records = []
+            for file in os.listdir(os.path.join(data_directory, 'FDDB-folds')):
+                if 'ellipse' in file:
+                    # Extract file information and create records using the extract_file_info function
+                    records = extract_file_info(data_directory,
+                                                os.path.join(data_directory, 'FDDB-folds', file),
+                                                create_record,
+                                                progress_bar)
+                    total_records.extend(records)
+        return total_records
+
+    urls = {
+        'originalPics.tar.gz': 'https://vis-www.cs.umass.edu/fddb/originalPics.tar.gz',
+        'FDDB-folds.tgz': 'https://vis-www.cs.umass.edu/fddb/FDDB-folds.tgz'
+    }
+    fddb_default = ImagesDataset(name='fddb',
+                                 urls=urls,
+                                 path=os.path.join('data', 'raw', 'FDDB'),
+                                 coco_file=os.path.join('data', 'raw', 'fddb.json'),
+                                 cache_directory=os.path.join('data', 'cache', 'fddb'))
+    fddb_default.generate_dataset_registration_info_params = {
+        'data_directory': fddb_default.path
+    }
+    fddb_default.registration_info_generator = generate_dataset_registration_info
+    return fddb_default
+
+
+if __name__ == '__main__':
+    data = get_fddb_dataset()
+    data.download_dataset_files()
+    data.generate_dataset_registration_info()
